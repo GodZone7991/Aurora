9eb1cf41c152029c619cc34b70744ccba128e490
GodZone7991 <rvasiliev39@gmail.com>
2023-10-19 00:24:15 +0300

    upload button update

--- appy.py
+++ appy.py
@@ -1,9 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import time
-import tempfile
 import os
-
-
 
 app = Flask(__name__)
 
@@ -12,28 +9,29 @@ def index():
     return render_template('main.html')
 
 @app.route('/upload', methods=['POST'])
-def upload_file(): 
+def upload_file():
     uploaded_file = request.files['csvFile']
-    # Specify the directory where you want to save the uploaded file
-    # Create a temporary directory in the current working directory
-    temp_dir = tempfile.mkdtemp(dir=os.getcwd())
-
-    # Get the filename of the uploaded file
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    upload_folder = os.path.join(current_directory, "uploads")
     filename = uploaded_file.filename
 
-    # Save the uploaded file in the temporary directory
-    file_path = os.path.join(temp_dir, filename)
+    if not os.path.exists(upload_folder):
+        os.makedirs(upload_folder)
+
+    file_path = os.path.join(upload_folder, filename)  # <- This line was missing in your code
     uploaded_file.save(file_path)
-
-
 
     questions = []
     for key, value in request.form.items():
         if key.startswith('question'):
             questions.append(value)
-    # Simulate processing time
-    time.sleep(5)
-    return f"File uploaded and questions received: {', '.join(questions)}"
+
+    time.sleep(5)  # Simulate processing time
+    return redirect(url_for('success'))
+
+@app.route('/success')
+def success():
+    return "File uploaded successfully"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,22 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const csvFile = document.getElementById('csvFile');
-    const progressBar = document.getElementById('progressBar');
-    
-    csvFile.addEventListener('change', function() {
+  const csvFile = document.getElementById('csvFile');
+  const progressBar = document.getElementById('progressBar');
+  const uploadMessage = document.getElementById('upload-message');
+
+  csvFile.addEventListener('change', function() {
       const file = csvFile.files[0];
       if (file) {
-        // Simulate file processing
-        let progress = 0;
-        const interval = setInterval(function() {
-          progress += 10;
-          progressBar.value = progress;
-          
-          if (progress >= 100) {
-            clearInterval(interval);
-            alert('File processing complete.');
-          }
-        }, 1000);
-      }
-    });
+          uploadMessage.innerText = `File: ${file.name} is uploaded`;
+          uploadMessage.classList.add('white-text'); // Add the white-text class to the uploadMessage element
+
+      // Simulate file processing
+      let progress = 0;
+      const interval = setInterval(function() {
+        progress += 10;
+        progressBar.value = progress;
+        
+        if (progress >= 100) {
+          clearInterval(interval);
+          alert('File processing complete.');
+        }
+      }, 1000);
+    }
   });
+});
+
   
